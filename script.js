4ff9be1a0829dd65ac5f216439b771d8b4ec168d
Roney Zakko <rooney.zakko@gmail.com>
2024-06-05 09:51:07 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,207 @@
+const dictionary = [
+    "appel", "boter", "cacao", "doorn", "eikel", "friet", "geest", "hond", 
+    "ijsje", "jager", "klomp", "lepel", "molen", "noten", "oase", "piano", 
+    "quilt", "roede", "sjaal", "uilen", "vogel", "wafel", "xenon", "yacht", 
+    "zeven", "akker", "akker", "banen", "beker", "beren", "chips", "diner", 
+    "draad", "eigen", "fiets", "garen",  "kabel", "kruis", "leven", 
+    "meter", "muren", "neven", "order", "paden", "prijs", "recht", "spijk", 
+    "tramp", "varen", "water", "weken", "zomer", "zwaar"
+];
+
+
+const targetWords = [
+    "appel", "boter", "cacao", "doorn", "eikel", "friet", "geest", "hond", 
+    "ijsje", "jager", "klomp", "lepel", "molen", "noten", "oase", "piano", 
+    "quilt", "roede", "sjaal", "uilen", "vogel", "wafel", "xenon", "yacht", 
+    "zeven", "akker", "banen", "beker", "beren", "chips", "diner", "draad", 
+    "eigen", "fiets", "garen", "kabel", "kruis", "leven", "meter", 
+    "muren", "neven", "order", "paden", "prijs", "recht", "spijk", "tramp", 
+    "varen", "water", "weken", "zomer", "zwaar"
+];
+;
+
+
+
+
+const WORD_LENGTH = 5;
+const FLIP_ANIMATION_DURATION = 500;
+const DANCE_ANIMATION_DURATION = 500;
+const keyboard = document.querySelector("[data-keyboard]");
+const alertContainer = document.querySelector("[data-alert-container]");
+const guessGrid = document.querySelector("[data-guess-grid]");
+const offsetFromDate = new Date(2022, 0, 1);
+const msOffset = Date.now() - offsetFromDate;
+const dayOffset = Math.floor(msOffset / 1000 / 60 / 60 / 24);
+const targetWord = targetWords[dayOffset % targetWords.length];
+
+startInteraction();
+
+function startInteraction() {
+  document.addEventListener("click", handleMouseClick);
+  document.addEventListener("keydown", handleKeyPress);
+}
+
+function stopInteraction() {
+  document.removeEventListener("click", handleMouseClick);
+  document.removeEventListener("keydown", handleKeyPress);
+}
+
+function handleMouseClick(e) {
+  if (e.target.matches("[data-key]")) {
+    pressKey(e.target.dataset.key);
+    return;
+  }
+
+  if (e.target.matches("[data-enter]")) {
+    submitGuess();
+    return;
+  }
+
+  if (e.target.matches("[data-delete]")) {
+    deleteKey();
+    return;
+  }
+}
+
+function handleKeyPress(e) {
+  if (e.key === "Enter") {
+    submitGuess();
+    return;
+  }
+
+  if (e.key === "Backspace" || e.key === "Delete") {
+    deleteKey();
+    return;
+  }
+
+  if (e.key.match(/^[a-z]$/i)) {
+    pressKey(e.key);
+    return;
+  }
+}
+
+function pressKey(key) {
+  const activeTiles = getActiveTiles();
+  if (activeTiles.length >= WORD_LENGTH) return;
+  const nextTile = guessGrid.querySelector(":not([data-letter])");
+  if (nextTile) {
+    nextTile.dataset.letter = key.toLowerCase();
+    nextTile.textContent = key;
+    nextTile.dataset.state = "active";
+  }
+}
+
+function deleteKey() {
+  const activeTiles = getActiveTiles();
+  const lastTile = activeTiles[activeTiles.length - 1];
+  if (lastTile) {
+    lastTile.textContent = "";
+    delete lastTile.dataset.state;
+    delete lastTile.dataset.letter;
+  }
+}
+
+function submitGuess() {
+  const activeTiles = [...getActiveTiles()];
+  if (activeTiles.length !== WORD_LENGTH) {
+    showAlert("Niet genoeg letters");
+    shakeTiles(activeTiles);
+    return;
+  }
+
+  const guess = activeTiles.reduce((word, tile) => {
+    return word + tile.dataset.letter;
+  }, "");
+
+  if (!dictionary.includes(guess)) {
+    showAlert("Niet in woordenlijst");
+    shakeTiles(activeTiles);
+    return;
+  }
+
+  stopInteraction();
+  activeTiles.forEach((tile, index) => flipTile(tile, index, activeTiles, guess));
+}
+
+function flipTile(tile, index, array, guess) {
+  const letter = tile.dataset.letter;
+  const key = keyboard.querySelector(`[data-key="${letter.toUpperCase()}"]`);
+  setTimeout(() => {
+    tile.classList.add("flip");
+  }, (index * FLIP_ANIMATION_DURATION) / 2);
+
+  tile.addEventListener("transitionend", () => {
+    tile.classList.remove("flip");
+    if (targetWord[index] === letter) {
+      tile.dataset.state = "correct";
+      key.classList.add("correct");
+    } else if (targetWord.includes(letter)) {
+      tile.dataset.state = "wrong-location";
+      key.classList.add("wrong-location");
+    } else {
+      tile.dataset.state = "wrong";
+      key.classList.add("wrong");
+    }
+
+    if (index === array.length - 1) {
+      tile.addEventListener("transitionend", () => {
+        startInteraction();
+        checkWinLose(guess, array);
+      }, { once: true });
+    }
+  }, { once: true });
+}
+
+function getActiveTiles() {
+  return guessGrid.querySelectorAll('[data-state="active"]');
+}
+
+function showAlert(message, duration = 1000) {
+  const alert = document.createElement("div");
+  alert.textContent = message;
+  alert.classList.add("alert");
+  alertContainer.prepend(alert);
+  if (duration == null) return;
+
+  setTimeout(() => {
+    alert.classList.add("hide");
+    alert.addEventListener("transitionend", () => {
+      alert.remove();
+    });
+  }, duration);
+}
+
+function shakeTiles(tiles) {
+  tiles.forEach(tile => {
+    tile.classList.add("shake");
+    tile.addEventListener("animationend", () => {
+      tile.classList.remove("shake");
+    }, { once: true });
+  });
+}
+
+function checkWinLose(guess, tiles) {
+  if (guess === targetWord) {
+    showAlert("Je hebt gewonnen!", 5000);
+    danceTiles(tiles);
+    stopInteraction();
+    return;
+  }
+
+  const remainingTiles = guessGrid.querySelectorAll(":not([data-letter])");
+  if (remainingTiles.length === 0) {
+    showAlert(`Het woord was: ${targetWord.toUpperCase()}`, null);
+    stopInteraction();
+  }
+}
+
+function danceTiles(tiles) {
+  tiles.forEach((tile, index) => {
+    setTimeout(() => {
+      tile.classList.add("dance");
+      tile.addEventListener("animationend", () => {
+        tile.classList.remove("dance");
+      }, { once: true });
+    }, (index * DANCE_ANIMATION_DURATION) / 5);
+  });
+}
